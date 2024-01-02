5d979108a55f63a48a475f79d1ac349a5d3b832c
Geanderson-Ferreira <geandersonferreiralves@gmail.com>
2024-01-02 01:08:23 -0300

    env

--- src/db_manager/migrations/env.py
+++ src/db_manager/migrations/env.py
@@ -4,8 +4,8 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from db_manager.models import Base
-from db_manager.config import DB 
+from src.db_manager.models import Base
+from src.db_manager.config import DB 
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
