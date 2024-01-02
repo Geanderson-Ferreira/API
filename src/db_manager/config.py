385d1aae85bf915923e83a4a7f19818065ec9281
Geanderson-Ferreira <geandersonferreiralves@gmail.com>
2024-01-02 02:18:35 -0300

    dot env working2

--- src/db_manager/config.py
+++ src/db_manager/config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE = os.getenv('DATABASE') #'sqlite:///example.db'
-DB_NAME = os.getenv('DB_NAME') #'example.db'
-MY_IP = os.getenv('MY_IP') #'10.0.0.102'
+DATABASE = os.getenv('DATABASE')
+DB_NAME = os.getenv('DB_NAME')
+MY_IP = os.getenv('MY_IP')
