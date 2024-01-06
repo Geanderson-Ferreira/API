d228e855db88a109a94138f4e7cdf1d9f9097e2d
Geanderson-Ferreira <geandersonferreiralves@gmail.com>
2024-01-05 23:29:00 -0300

    com teste

--- api.py
+++ api.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from src.routers.orderRouters import router as order_router
 from src.routers.userRouters import router as user_router
 
+#ale
+
 #Para rodar a API, esteja no mesmo diretorio desde arquivo e rodar:
 #>> uvicorn api:app
 #>> uvicorn api:app --host 10.0.0.102 --port 8000 --reload
