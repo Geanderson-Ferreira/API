872cd28262313c183b195c138a49e61299570083
Geanderson-Ferreira <geandersonferreiralves@gmail.com>
2024-01-08 16:08:42 -0300

    starlette implemented

--- api.py
+++ api.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 from src.routers.orderRouters import router as order_router
 from src.routers.userRouters import router as user_router
-from src.routers.adminRouters import router as admin_router
-
 from src.admin_way.starlette import admin
+
 from fastapi import FastAPI
-from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
@@ -13,5 +11,4 @@ app.title = 'Projeto App Care'
 
 app.include_router(order_router)
 app.include_router(user_router)
-app.include_router(admin_router)
-#admin.mount_to(app)
+admin.mount_to(app)
